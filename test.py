938a3c0fa1c8d35a6c4dac4f09d3a0e749f5b0aa
maher14879 <maherelyoussef1@gmail.com>
2024-05-31 13:39:42 +0200

    just the basics

---
+++ test.py
@@ -0,0 +1,6 @@
+kebabs = [
+    {"Name": "Chicken Kebab", "Price": 5.99, "Spicy": True},
+    {"Name": "Beef Kebab", "Price": 6.99, "Spicy": False},
+    {"Name": "Lamb Kebab", "Price": 7.99, "Spicy": True},
+    {"Name": "Vegetable Kebab", "Price": 4.99, "Spicy": False}
+]
--- visualize.py
+++ visualize.py
@@ -1,10 +1,11 @@
-
-
+import pandas as pd
+import pretty_html_table
 
 def html_from_list(kebabs: list[dict]) -> str:
     results = {}
     for kebab in kebabs:
         for key in kebab.keys():
-            if results[key]: results[key] += kebab[key]
+            if key in results.keys(): results[key] += [kebab[key]]
             else: results[key] = [kebab[key]]
-    
+    df = pd.DataFrame(results)
+    return pretty_html_table.build_table(df, 'blue_light')
